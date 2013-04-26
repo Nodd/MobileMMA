5940dbee89b09afc2449ca971ef1fd40e00167ea
Joseph Martinot-Lagarde <joseph.martinot-lagarde@onera.fr>
2013-04-26 22:05:43 +0200

    Necessary files to launch MobileMMA with kivy-launcher

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals
+"""
+This module is necessary to launch MobileMMA with kivy launcher.
+"""
+
+from mmamobile import MMAMobileApp
+
+
+if __name__ == "__main__":
+    MMAMobileApp().run()
