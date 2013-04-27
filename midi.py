a975699544cccdeef4129df74bceccd0a6449a80
Joseph Martinot-Lagarde <joseph.martinot-lagarde@onera.fr>
2013-04-27 16:02:55 +0200

    Added logging and some corrections

--- midi.py
+++ midi.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, unicode_literals
 """Functions to build and play midi files from mma files.
@@ -9,62 +9,80 @@ Most code is adapted from LeMMA.
 import os
 import sys
 import subprocess
+import logging
 
-import pygame
-pygame.init()
-pygame.mixer.init()
-pygame.mixer.music.set_volume(1.0)
+try:
+    import pygame
+    pygame.init()
+    import pygame.mixer as mixer
+except ImportError:
+    import android_mixer as mixer
+mixer.init()
+mixer.music.set_volume(1.0)
+
+#from kivy.core.audio import SoundLoader
 
 PYTHON = sys.executable
 MMA_EXECFILE = os.path.join(os.path.dirname(__file__), "mma/mma.py")
 MMA_PATH = os.path.dirname(MMA_EXECFILE)
 
 
-def build(filename):
+def build(filename_mma):
     """Run the MMA program for the given filename and returns the filename of
     the MIDI file created.
 
     Even if MMA is written in python, it is sadly far easier to run in with
     popen().
     """
-    assert filename.lower().endswith(".mma")
-    assert os.path.isfile(filename)
-    assert os.access(filename, os.R_OK)
+    assert filename_mma.lower().endswith(".mma")
+    assert os.path.isfile(filename_mma)
+    assert os.access(filename_mma, os.R_OK)
 
     # The working diretory will be temporarilly changed
-    filename = os.path.abspath(filename)
+    filename_mma = os.path.abspath(filename_mma)
+
+    filename_midi = filename_mma[:-4] + ".mid"
+    logging.debug("Building midifile from %s to %s" %
+                  (filename_mma, filename_midi))
 
     # MMA must run from mma.py path
     current_dir = os.getcwd()
     os.chdir(MMA_PATH)
-
+    logging.debug("cd from %s to %s" % (current_dir, MMA_PATH))
     try:
-        # delete the temp midi file first, if any
-        tempmidifile = os.path.normcase(current_dir + "/_temp_.mid")
         try:
-            os.remove(tempmidifile)
+            os.remove(filename_midi)
+            logging.debug("Removed %s" % filename_midi)
         except Exception:
+            logging.debug("No midi file to remove")
             pass
 
         # Generate the midi file
         #    quote pythonPath to handle correctly space characters in it
-        cmd = os.path.normcase("'%s' %s %s"
-                               % (sys.executable, MMA_EXECFILE, filename))
-        print(subprocess.check_call(cmd, shell=True))
-    except subprocess.CalledProcessError:
+        cmd = os.path.normcase("%s %s"
+                               % (MMA_EXECFILE, filename_mma))
+        logging.debug("Running \"%s\"" % cmd)
+        errcode = subprocess.check_call(cmd, shell=True)
+        logging.debug("MMA returned error code %d" % errcode)
+    except Exception as err:
+        logging.error("Error running MMA")
+        logging.exception(err)
         raise
     finally:
         # Back to the previous working dir
+        logging.debug("cd back to %s" % (current_dir))
         os.chdir(current_dir)
 
-    return filename[:-4] + ".mid"
+    return filename_midi
 
 
 def play(filename=None):
     """Plays a MIDI file using pygame
     """
+    logging.debug("Play %s" % filename)
     if filename:
         if filename.lower().endswith(".mma"):
+            logging.debug("MMA file, building MIDI...")
             assert os.path.isfile(filename)
             assert os.access(filename, os.R_OK)
             filename = build(filename)
@@ -73,19 +91,30 @@ def play(filename=None):
         assert os.path.isfile(filename)
         assert os.access(filename, os.R_OK)
 
-        pygame.mixer.music.load(filename)
-        pygame.mixer.music.play()
+#        sound = SoundLoader.load(filename)
+#        print(sound)
+#        if sound:
+#            print("Sound found at %s" % sound.source)
+#            print("Sound is %.3f seconds" % sound.length)
+#        sound.play()
+#        return sound
+
+        logging.debug("Play %s" % filename)
+        mixer.music.load(filename)
+        mixer.music.play()
     else:
-        pygame.mixer.music.unpause()
+        mixer.music.unpause()
 
 
 def stop():
     """Stop the midifile play
     """
-    pygame.mixer.music.stop()
+    logging.debug("Stop playing")
+    mixer.music.stop()
 
 
 def pause():
     """Pause the midifile play
     """
-    pygame.mixer.music.pause()
+    logging.debug("Pause playing")
+    mixer.music.pause()
--- mobilemma.py
+++ mobilemma.py
@@ -1,8 +1,27 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, unicode_literals
 """
 """
+
+import logging
+# création de l'objet logger qui va nous servir à écrire dans les logs
+logger = logging.getLogger()
+# on met le niveau du logger à DEBUG, comme ça il écrit tout
+logger.setLevel(logging.DEBUG)
+
+# création d'un formateur qui va ajouter le temps, le niveau
+# de chaque message quand on écrira un message dans le log
+formatter = logging.Formatter(
+    "%(levelname)s :: %(funcName)s:%(lineno)d :: %(message)s")
+# création d'un handler qui va rediriger une écriture du log vers
+# un fichier en mode 'append', avec 1 backup et une taille max de 1Mo
+file_handler = logging.FileHandler('activity.log', mode='w', encoding="utf8")
+# on lui met le niveau sur DEBUG, on lui dit qu'il doit utiliser le formateur
+# créé précédement et on ajoute ce handler au logger
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 from kivy.app import App
 from kivy.uix.widget import Widget
@@ -14,16 +33,25 @@ class MobileMMAUI(Widget):
     def play_pause(self, filename):
         """Play the selected filename.
         """
-        midi.play(filename)
+        logging.debug("Play button pressed")
+        try:
+            midi.play(filename)
+        except Exception as err:
+            logging.exception(err)
 
     def stop(self):
         """Stop playing.
         """
+        logging.debug("Stop button pressed")
         midi.stop()
 
     def file_content(self, filename):
         """Return content of a mma test file.
         """
+        logging.debug("Retrieve content of %s" % filename)
+        if filename.endswith(('.mid', '.midi')):
+            return "MIDI file"
+
         assert filename.lower().endswith(".mma")
         with open(filename, "rt") as fid:
             text = fid.read()
@@ -32,6 +60,7 @@ class MobileMMAUI(Widget):
 
 class MobileMMAApp(App):
     def build(self):
+        logging.debug("Create Kivy UI")
         return MobileMMAUI()
 
 
