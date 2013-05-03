9d007860726102a0dee00bcf56363318b6bc3e18
Joseph Martinot-Lagarde <joseph.martinot-lagarde@onera.fr>
2013-05-03 23:59:36 +0200

    Use MMA internal vars for lexer and avoid import *

--- mmalexer.py
+++ mmalexer.py
@@ -8,7 +8,7 @@ Created on Sat Jan 19 14:57:57 2013
 
 
 from pygments.lexer import RegexLexer
-from pygments.token import *
+import pygments.token as tkn
 
 from MMA.chordtable import chordlist
 # Get the chords, join them as a OR regexp and remove leading and trailing
@@ -17,6 +17,13 @@ chords = repr('|'.join(sorted(chordlist.iterkeys()))).strip("'")
 # Escape problematic chars in chords
 for char in "+#()":
     chords = chords.replace(char, '\\' + char)
+
+from MMA.parse import simpleFuncs, trackFuncs
+commands = '|'.join(sorted(simpleFuncs.iterkeys()))
+track_commands = '|'.join(sorted(trackFuncs.iterkeys()))
+
+from MMA.alloc import trkClasses
+track_classes = '|'.join(sorted(trkClasses.iterkeys()))
 
 
 class MMALexer(RegexLexer):
@@ -33,38 +40,14 @@ class MMALexer(RegexLexer):
 
     tokens = {
         'root': [
-            (r'//.*\n', Comment.Single),
-            (r'^\d+\b', Comment),
-            # Taken from mma -Dk
+            (r'//.*\n', tkn.Comment.Single),
+            (r'^\d+\b', tkn.Comment),
             # (?i) is for case insensitive
-            (r'(?i)\b(ADJUSTVOLUME|ALLGROOVES|ALLTRACKS|AUTHOR|AUTOSOLOTRACKS|'
-             r'BEATADJUST|BEGIN|CHANNELPREF|CHORDADJUST|COMMENT|CRESC|CUT|'
-             r'DEBUG|DEC|DECRESC|DEFALIAS|DEFCHORD|DEFGROOVE|DELETE|DOC|DOCVAR|'
-             r'DRUMVOLTR|ELSE|END|ENDIF|ENDMSET|ENDREPEAT|EOF|FERMATA|GOTO|'
-             r'GROOVE|GROOVECLEAR|IF|IFEND|INC|INCLUDE|KEYSIG|LABEL|LYRIC|MIDI|'
-             r'MIDICOPYRIGHT|MIDICUE|MIDIDEF|MIDIFILE|MIDIINC|MIDIMARK|'
-             r'MIDISPLIT|MIDITEXT|MIDITNAME|MMAEND|MMASTART|MSET|MSETEND|'
-             r'NEWSET|PATCH|PRINT|PRINTACTIVE|PRINTCHORD|REPEAT|REPEATEND|'
-             r'REPEATENDING|RESTART|RNDSEED|RNDSET|SEQ|SEQCLEAR|SEQRND|'
-             r'SEQRNDWEIGHT|SEQSIZE|SET|SETINCPATH|SETLIBPATH|SETMIDIPLAYER|'
-             r'SETOUTPATH|SETSYNCTONE|SHOWVARS|STACKVALUE|SWELL|SWINGMODE|'
-             r'SYNCHRONIZE|TEMPO|TIME|TIMESIG|TONETR|TRANSPOSE|TRUNCATE|TWEAKS|'
-             r'UNSET|USE|VARCLEAR|VEXPAND|VOICETR|VOICEVOLTR|VOLUME|'
-             r'ACCENT|ARPEGGIATE|ARTICULATE|CAPO|CHANNEL|CHORDS|CHSHARE|'
-             r'COMPRESS|COPY|CRESC|CUT|DECRESC|DEFINE|DELAY|DIRECTION|DRUMTYPE|'
-             r'DUPRIFF|DUPROOT|FORCEOUT|GROOVE|HARMONY|HARMONYONLY|'
-             r'HARMONYVOLUME|INVERT|LIMIT|MALLET|MIDICLEAR|MIDICRESC|MIDICUE|'
-             r'MIDIDECRESC|MIDIDEF|MIDIGLIS|MIDINOTE|MIDIPAN|MIDISEQ|MIDITEXT|'
-             r'MIDITNAME|MIDIVOICE|MIDIVOLUME|MOCTAVE|NOTESPAN|OCTAVE|OFF|ON|'
-             r'ORNAMENT|RANGE|RESTART|RIFF|RSKIP|RTIME|RVOLUME|'
-             r'SCALETYPE|SEQCLEAR|SEQRND|SEQRNDWEIGHT|SEQUENCE|STRETCH|STRUM|'
-             r'SWELL|TONE|TUNING|UNIFY|VOICE|VOICING|VOLUME)\s*\b',
-             Keyword),
-            (r'\b(ARIA|ARPEGGIO|BASS|CHORD|DRUM|MELODY|PLECTRUM|SCALE|SOLO|'
-             r'WALK)\s*\b(?!\.)',
-             Name.Builtin),
-            (r'[*]', Operator),
-            (r'[{}]', Punctuation),
-            (r'\b(/|z|[A-G][b#]?(' + chords + r')?)\b', Name.Entity),
+            (r'(?i)\b(' + commands + r')\b', tkn.Keyword),
+            (r'(?i)\b(' + track_commands + r')\b', tkn.Keyword),
+            (r'(?i)\b(' + track_classes + r')\b', tkn.Name.Builtin),
+            (r'[*]', tkn.Operator),
+            (r'[{}]', tkn.Punctuation),
+            (r'\b(/|z|[A-G][b#]?(' + chords + r')?)\b', tkn.String.Symbol),
         ]
     }
